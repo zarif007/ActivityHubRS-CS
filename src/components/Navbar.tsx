435b5e9e30dd7b4d9e9ed2c96e2adf928346117e
zarif007 <zarifhuq786@gmail.com>
2023-05-17 16:45:57 +0600

    Added seminars and workshops options

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import Image from "next/image";
 import React from "react";
@@ -8,42 +8,58 @@ import { useRouter } from "next/navigation";
 import { BsPersonWorkspace } from "react-icons/bs";
 
 const Navbar = () => {
-
   const router = useRouter();
 
   const styles = {
     wrapper: `p-2 fixed max-w-7xl mx-auto w-full backdrop-blur-sm z-50 top-0 left-0 right-0 h-16 md:h-20 shadow-sm 
       flex items-center justify-between rounded-sm`,
-    optionStyles: `flex space-x-2 items-center justify-center shadow-2xl font-extrabold text-2xl 
+    optionStyles: `flex space-x-2 items-center justify-center shadow-2xl font-extrabold text-4xl md:text-2xl
       text-white cursor-pointer no-underline hover:underline decoration-indigo-500`,
   };
 
   return (
     <div className={styles.wrapper}>
-
-      {/* Logo */}
-      <Image src="/logo.png" className="cursor-pointer" height="60" width="60" alt="logo" onClick={() => router.push('/')} />
-
-      {/* Options on Navbar */}
-      <div className="flex space-x-3 md:space-x-8">
-        <h1 className={styles.optionStyles} onClick={() => router.push('/activities')} >
+      <div className="flex space-x-4 md:space-x-8">
+        <h1
+          className={styles.optionStyles}
+          onClick={() => router.push("/activities")}
+        >
           <GiStairsGoal />
           <p className="hidden md:flex">Activities</p>
         </h1>
-        <h1 className={styles.optionStyles} onClick={() => router.push('/registeractivity')} >
+        <h1
+          className={styles.optionStyles}
+          onClick={() => router.push("/registeractivity")}
+        >
           <BiLogIn />
           <p className="hidden md:flex">Register</p>
         </h1>
-        <h1 className={styles.optionStyles} onClick={() => router.push('/registeractivity')} >
+      </div>
+      {/* Logo */}
+      <Image
+        src="/logo.png"
+        className="cursor-pointer bg-gray-200 rounded"
+        height="50"
+        width="50"
+        alt="logo"
+        onClick={() => router.push("/")}
+      />
+      <div className="flex space-x-4 md:space-x-8">
+        <h1
+          className={styles.optionStyles}
+          onClick={() => router.push("/registeractivity")}
+        >
           <GiPublicSpeaker />
           <p className="hidden md:flex">Seminars</p>
         </h1>
-        <h1 className={styles.optionStyles} onClick={() => router.push('/registeractivity')} >
+        <h1
+          className={styles.optionStyles}
+          onClick={() => router.push("/registeractivity")}
+        >
           <BsPersonWorkspace />
           <p className="hidden md:flex">Workshops</p>
         </h1>
       </div>
-
     </div>
   );
 };
