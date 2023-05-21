4a18aabc1c655e6cab6d63b49907b6cc5f761b2f
zarif007 <zarifhuq786@gmail.com>
2023-05-22 01:24:53 +0600

    Added seat status and made pages CSR in activities pages

--- src/app/(activities)/activities/[id]/page.tsx
+++ src/app/(activities)/activities/[id]/page.tsx
@@ -1,7 +1,11 @@
+'use client'
+
 import { apiEndpointV1 } from "@/lib/ApiEndpoints";
 import { ActivityInterface } from "@/types/activity";
 import axios from "axios";
-import React from "react";
+import React,{ useEffect, useState } from "react";
+import Loading from "./loading";
+import { ActivityStateInterface } from "@/types/activityState";
 
 interface PageParams {
   params: {
@@ -9,69 +13,85 @@ interface PageParams {
   };
 }
 
-const Activity = async ({ params }: PageParams) => {
-  const res = await axios.get(`${apiEndpointV1}/activity/${params.id}`);
-  const activity: ActivityInterface = res.data.data;
+const Activity = ({ params }: PageParams) => {
+  const [activityState, setActivityState] = useState<ActivityStateInterface | null>(null);
+  
+  useEffect(() => {
+    const getData = async () => {
+      const res = await axios.get(`${apiEndpointV1}/activitystate/${params.id}`)
+      setActivityState(res.data.data)
+    }
+    getData();
+  }, [])
 
   return (
-    <section className="text-gray-400 bg-gray-900 body-font">
-      <div className="container px-5 py-24 mx-auto flex flex-col">
-        <div className="lg:w-4/6 mx-auto">
-          <div className="rounded-lg h-80 w-80 mx-auto overflow-hidden">
-            <img
-              alt="Activity Image"
-              className="object-center h-full w-full"
-              src={activity.image}
-            />
-          </div>
-          <div className="flex flex-col-reverse sm:flex-row mt-10">
-            <div className="sm:w-1/3 text-center sm:pr-8 sm:py-8">
-              <div className="w-20 h-20 rounded-full inline-flex items-center justify-center bg-gray-800 text-gray-600">
-                {!activity.instructor.image ? (
-                  <img
-                    alt="content"
-                    className="object-center h-full w-full"
-                    src={activity.instructor.image}
-                  />
-                ) : (
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    className="w-10 h-10"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
-                    <circle cx="12" cy="7" r="4"></circle>
-                  </svg>
-                )}
+    <>
+      {
+        activityState ? <section className="text-gray-400 bg-gray-900 body-font">
+        <div className="container px-5 py-24 mx-auto flex flex-col">
+          <div className="lg:w-4/6 mx-auto">
+            <div className="rounded-lg h-80 w-80 mx-auto overflow-hidden">
+              <img
+                alt="Activity Image"
+                className="object-center h-full w-full"
+                src={activityState.activityId.image}
+              />
+            </div>
+            <div className="flex flex-col-reverse sm:flex-row mt-10">
+              <div className="sm:w-1/3 text-center sm:pr-8 sm:py-8">
+                <div className="w-20 h-20 rounded-full inline-flex items-center justify-center bg-gray-800 text-gray-600">
+                  {activityState.activityId.instructor.image ? (
+                    <img
+                      alt="content"
+                      className="object-center h-full w-full"
+                      src={activityState.activityId.instructor.image}
+                    />
+                  ) : (
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      className="w-10 h-10"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+                      <circle cx="12" cy="7" r="4"></circle>
+                    </svg>
+                  )}
+                </div>
+                <div className="flex flex-col items-center text-center justify-center">
+                  <h2 className="font-medium title-font mt-4 text-white text-lg">
+                    {activityState.activityId.instructor.fullName}
+                  </h2>
+                  <div className="w-12 h-1 bg-indigo-500 rounded mt-2 mb-4"></div>
+                  <p className="text-base text-gray-400">
+                    {activityState.activityId.instructor.details}
+                  </p>
+                </div>
               </div>
-              <div className="flex flex-col items-center text-center justify-center">
-                <h2 className="font-medium title-font mt-4 text-white text-lg">
-                  {activity.instructor.fullName}
-                </h2>
-                <div className="w-12 h-1 bg-indigo-500 rounded mt-2 mb-4"></div>
-                <p className="text-base text-gray-400">
-                  {activity.instructor.details}
+              <div className="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-800 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
+                <h1 className="my-2 font-extrabold text-2xl md:text-4xl text-white">
+                  {activityState.activityId.name}
+                </h1>
+                <div className="w-24 h-1 bg-indigo-500 rounded mt-2 mb-4 mx-auto sm:mx-0"></div>
+                <h3 className="tracking-widest text-indigo-400 text-sm font-medium title-font">
+                  {activityState.activityId.day}({activityState.activityId.classTime}) at {activityState.activityId.venue}
+                </h3>
+                <h3 className="tracking-widest text-indigo-400 text-sm font-medium title-font">
+                  Seat Status: {activityState.bookedSeat}/{activityState.totalSeat}
+                </h3>
+                <p className="leading-relaxed text-lg mb-4">
+                  {activityState.activityId.description}
                 </p>
               </div>
             </div>
-            <div className="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-800 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
-              <h1 className="my-2 font-extrabold text-2xl md:text-4xl text-white">{activity.name}</h1>
-              <div className="w-24 h-1 bg-indigo-500 rounded mt-2 mb-4 mx-auto sm:mx-0"></div>
-              <h3 className="tracking-widest text-indigo-400 text-sm font-medium title-font">
-                {activity.day}({activity.classTime}) at {activity.venue}
-              </h3>
-              <p className="leading-relaxed text-lg mb-4">
-                {activity.description}
-              </p>
-            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section> : <Loading />
+      }
+    </>
   );
 };
 
--- src/app/(activities)/activities/page.tsx
+++ src/app/(activities)/activities/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import AIActivitySuggestionModal from "@/components/AIActivitySuggestionModal";
 import { colorSchema } from "@/lib/ColorSchema";
 import { apiEndpointV1 } from "@/lib/ApiEndpoints";
@@ -5,72 +6,96 @@ import { ActivityInterface } from "@/types/activity";
 import axios from "axios";
 import { Metadata } from "next";
 import Link from "next/link";
-import { notFound } from "next/navigation";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { ActivityStateInterface } from "@/types/activityState";
+import Loading from "./loading";
 
 export const metadata: Metadata = {
   title: "RS Activity || Activities",
-  description: "Rs Activities"
-}
+  description: "Rs Activities",
+};
 
-const Activities = async () => {
-
+const Activities = () => {
   const styles = {
     button: `${colorSchema.button} mt-2 flex py-2 w-full max-w-lg font-extrabold text-xl rounded-sm items-center justify-center space-x-2 hover:space-x-4 `,
   };
 
-  const activities = await axios.get(`${apiEndpointV1}/activity`)
+  const [activityStates, setActivityStates] = useState<
+    ActivityStateInterface[] | null
+  >(null);
 
-  if(!activities) return notFound()
-  
+  useEffect(() => {
+    const getData = async () => {
+      const res = await axios.get(`${apiEndpointV1}/activitystate`);
+      setActivityStates(res.data.data);
+    };
+    getData();
+  }, []);
+
   return (
     <div className="mt-16 md:mt-24 w-full max-w-7xl mx-auto bg-gray-900">
-      
       <AIActivitySuggestionModal />
 
-      <section className="text-gray-400 body-font">
-        <div className="container px-5 py-24 mx-auto">
-          <div className="flex flex-wrap w-full mb-20">
-            <div className="lg:w-1/2 w-full mb-6 lg:mb-0">
-              <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-white">
-                RS Activities
-              </h1>
-              <div className="h-1 w-20 bg-indigo-500 rounded"></div>
+      {activityStates ? (
+        <section className="text-gray-400 body-font">
+          <div className="container px-5 py-24 mx-auto">
+            <div className="flex flex-wrap w-full mb-20">
+              <div className="lg:w-1/2 w-full mb-6 lg:mb-0">
+                <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-white">
+                  RS Activities
+                </h1>
+                <div className="h-1 w-20 bg-indigo-500 rounded"></div>
+              </div>
+            </div>
+            <div className="flex flex-wrap -m-4">
+              {activityStates.length > 0 &&
+                activityStates.map(
+                  (activityState: ActivityStateInterface, index: number) => {
+                    return (
+                      <div key={index} className="xl:w-1/4 md:w-1/2 w-full p-4">
+                        <div className="bg-gray-800 bg-opacity-40 p-6 rounded-lg">
+                          <img
+                            className="h-40 rounded w-full object-cover object-center mb-6"
+                            src={activityState.activityId.image}
+                            alt="Activity Image"
+                            style={{ objectFit: "contain" }}
+                          />
+                          <h3 className="tracking-widest text-indigo-400 text-xs font-medium title-font uppercase">
+                            {activityState.activityId.day}(
+                            {activityState.activityId.classTime})
+                          </h3>
+                          <Link
+                            href={`/activities/${activityState.activityId._id}`}
+                          >
+                            <div className="text-lg text-white font-medium title-font mb-4 h-12">
+                              {activityState.activityId.name}
+                            </div>
+                          </Link>
+                          <p className="leading-relaxed text-base h-20">
+                            {activityState.activityId.description.slice(0, 60)}
+                            ...
+                          </p>
+                          
+                          <h3 className="tracking-widest text-indigo-400 text-xs font-medium title-font uppercase">
+                            Seat Status: {activityState.bookedSeat}/{activityState.totalSeat}
+                          </h3>
+                          <Link
+                            href={`/activities/${activityState.activityId._id}`}
+                            className={styles.button}
+                          >
+                            Details
+                          </Link>
+                        </div>
+                      </div>
+                    );
+                  }
+                )}
             </div>
           </div>
-          <div className="flex flex-wrap -m-4">
-            {
-              activities.data.data.length > 0 && activities.data.data.map((activity: ActivityInterface, index: number) => {
-                return (
-                  <div key={index} className="xl:w-1/4 md:w-1/2 w-full p-4">
-                    <div className="bg-gray-800 bg-opacity-40 p-6 rounded-lg">
-                      <img
-                        className="h-40 rounded w-full object-cover object-center mb-6"
-                        src={activity.image}
-                        alt="Activity Image"
-                        style={{ objectFit: 'contain' }}
-                      />
-                      <h3 className="tracking-widest text-indigo-400 text-xs font-medium title-font uppercase">
-                        {activity.day}({activity.classTime})
-                      </h3>
-                      <Link href={`/activities/${activity._id}`} >
-                        <div className="text-lg text-white font-medium title-font mb-4 h-12">
-                          {activity.name}
-                        </div>
-                      </Link>
-                      <p className="leading-relaxed text-base h-20">
-                        {activity.description.slice(0, 60)}...
-                      </p>
-
-                      <Link href={`/activities/${activity._id}`} className={styles.button} >Details</Link>
-                    </div>
-                  </div>
-                )
-              })
-            }
-          </div>
-        </div>
-      </section>
+        </section>
+      ) : (
+        <Loading />
+      )}
     </div>
   );
 };
