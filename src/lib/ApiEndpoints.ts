8effa1e9124ee849a60d1de2a6276bdfa6e83e18
Danial Islam Shohardo <danialcodes@gmail.com>
2023-05-28 12:39:26 +0600

    Update

--- src/lib/ApiEndpoints.ts
+++ src/lib/ApiEndpoints.ts
@@ -1,2 +1,2 @@
-// export const apiEndpointV1 = 'https://rsbackend.skillsnai.xyz/api/v1';
-export const apiEndpointV1 = 'http://localhost:5000/api/v1';
+export const apiEndpointV1 = 'https://rsbackend.skillsnai.xyz/api/v1';
+// export const apiEndpointV1 = 'http://localhost:5000/api/v1';
