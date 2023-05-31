952bf74a3f2c25db1339e9773c8f9504ccddab28
zarif007 <zarifhuq786@gmail.com>
2023-06-01 01:32:39 +0600

    Added overall seatstatus

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,20 @@
-'use client'
-
 import Navbar from "@/components/Navbar";
 import "./globals.css";
-import "./nprogress.css";
 import { Inter } from "next/font/google";
 import Colorhydration from "@/components/Colorhydration";
 import { colorSchema } from "@/lib/ColorSchema";
 import { Toaster } from "@/components/ui/Toast";
 import GoogleAnalytics from "@/components/GoogleAnalytics";
 import { Analytics } from "@vercel/analytics/react";
-import AuthProviders from "@/components/AuthProviders";
-
-import nProgress from 'nprogress'
-import Router from 'next/router'
+import { ClerkProvider } from "@clerk/nextjs";
 
 
-Router.events.on('routeChangeStart', nProgress.start);
-Router.events.on('routeChangeError', nProgress.done);
-Router.events.on('routeChangeComplete', nProgress.done);
+const inter = Inter({ subsets: ["latin"] });
 
-// const inter = Inter({ subsets: ["latin"] });
-
-// export const metadata = {
-//   title: "Activity Hub RS",
-//   description: "Activity Hub RS",
-// };
+export const metadata = {
+  title: "Activity Hub RS",
+  description: "Activity Hub RS",
+};
 
 export default function RootLayout({
   children,
@@ -33,14 +23,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${colorSchema.background}`}>
-      <GoogleAnalytics />
-      <Analytics />
-      <Colorhydration />
-      <Toaster position="bottom-center" />
-      <AuthProviders>
+      
+        <GoogleAnalytics />
+        <Analytics />
+        <Colorhydration />
+        <Toaster position="bottom-center" />
         <Navbar />
         <body>{children}</body>
-      </AuthProviders>
     </html>
   );
 }
